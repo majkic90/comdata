3a394f273424fade6eacdbff736a026a0e015507
Aleksandar Majkiac <Aleksandar Majkiac>
2017-02-24 09:21:00 +0100

    Initinig

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-var io = require('socket.io').listen(8080);
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var PORT = process.env.PORT || 8080;
+
 io.on('fontStyle', function(data){
     io.emit('fontStyle', data);
 });
+
+http.listen(PORT, function () {
+    console.log('listen', PORT);
+})
